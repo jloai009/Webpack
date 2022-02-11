bc9702fa1d000cc37609f957e0481d45fbcef759
Jose <jloai009@fiu.edu>
2022-02-11 14:44:16 -0500

    Add PromisePolyfill support

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
